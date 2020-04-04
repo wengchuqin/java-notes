016ace91e616baec1ddfb0fe1db7e207c7fac7c2
wengchuqin <wengchuqin@meituan.com>
2020-04-04 17:22:14 +0800

    精简代码

--- src/test/java/wcq/learning/java/notes/algorithm/sort/HeapSort.java
+++ src/test/java/wcq/learning/java/notes/algorithm/sort/HeapSort.java
@@ -43,8 +43,8 @@ public class HeapSort implements Sort{
         }
     }
 
-    // To heapify a subtree rooted with node i which is
-    // an index in arr[]. n is size of heap
+    // To heapify a subtree rooted with node i which is an index in arr[].
+    // n is size of heap
     void heapify(int arr[], int n, int i) {
         int largest = i; // Initialize largest as root
         int l = 2 * i + 1; // left = 2*i + 1
@@ -60,9 +60,8 @@ public class HeapSort implements Sort{
 
         // If largest is not root
         if (largest != i) {
-            int swap = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = swap;
+
+            change(arr, i, largest);
 
             // Recursively heapify the affected sub-tree
             heapify(arr, n, largest);
